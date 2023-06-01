dea7b57e84e778cd927f011a678483633e0123f7
David Soards <david@soards.me>
2023-06-01 02:14:49 -0400

    add Avatar

---
+++ src/components/DAvatar/DAvatar.story.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import DAvatar from './DAvatar.vue';
+</script>
+
+<template>
+  <Story :layout="{ type: 'grid', width: 300 }">
+    <Variant title="No Avatar">
+      <DAvatar />
+    </Variant>
+    <Variant title="Image with Notif">
+      <DAvatar
+        image="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+        :notifications="1"
+      />
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/DAvatar/DAvatar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    image?: string;
+    notifications?: number;
+  }>(),
+  { notifications: 0 },
+);
+
+const hasNotif = computed(() => props.notifications > 0);
+</script>
+
+<template>
+  <span class="relative inline-block">
+    <img v-if="props.image" class="h-10 w-10 rounded-full" :src="props.image" alt="" />
+    <svg
+      v-else
+      class="h-10 w-10 rounded-full bg-gray-100 text-gray-300"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
+      />
+    </svg>
+    <span
+      v-if="hasNotif"
+      class="absolute bottom-0 right-0 block h-2.5 w-2.5 rounded-full bg-green-400 ring-2 ring-white"
+    />
+  </span>
+</template>
+
+<style scoped></style>
