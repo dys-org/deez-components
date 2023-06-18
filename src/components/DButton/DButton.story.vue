f6aa499315d38c9f48990f11220c18d467c8e863
David Soards <david@soards.me>
2023-06-18 11:43:53 -0400

    revising Button

--- src/components/DButton/DButton.story.vue
+++ src/components/DButton/DButton.story.vue
@@ -5,11 +5,38 @@ import { logEvent } from 'histoire/client';
 
 <template>
   <Story :layout="{ type: 'grid', width: 300 }">
-    <Variant title="Primary">
-      <DButton @click="logEvent('Clicked Primary', $event)"> Click Me </DButton>
+    <Variant title="Default">
+      <DButton @click="logEvent('Clicked Default', $event)">Click Me</DButton>
     </Variant>
-    <Variant title="Danger">
-      <DButton @click="logEvent('Clicked Danger', $event)" danger> Click Me </DButton>
+
+    <Variant title="Primary">
+      <DButton variant="primary" @click="logEvent('Clicked Primary', $event)">Click Me</DButton>
+    </Variant>
+
+    <Variant title="Default Danger">
+      <DButton @click="logEvent('Clicked Default Danger', $event)" danger>Click Me</DButton>
+    </Variant>
+
+    <Variant title="Primary Danger">
+      <DButton variant="primary" danger @click="logEvent('Clicked Primary Danger', $event)">
+        Click Me
+      </DButton>
+    </Variant>
+
+    <Variant title="Text">
+      <DButton variant="text" @click="logEvent('Clicked Text', $event)">Click Me</DButton>
+    </Variant>
+
+    <Variant title="Text Danger">
+      <DButton variant="text" danger @click="logEvent('Clicked Text Danger', $event)"
+        >Click Me</DButton
+      >
+    </Variant>
+
+    <Variant title="Primary Loading">
+      <DButton variant="primary" loading @click="logEvent('Clicked Primary Loading', $event)">
+        Click Me
+      </DButton>
     </Variant>
   </Story>
 </template>
--- src/components/DButton/DButton.vue
+++ src/components/DButton/DButton.vue
@@ -2,33 +2,117 @@
 import { computed } from 'vue';
 
 export interface DButtonProps {
-  variant?: 'primary' | 'secondary' | 'tertiary' | null;
+  variant?: 'default' | 'primary' | 'text';
   danger?: boolean;
   loading?: boolean;
+  type?: 'button' | 'submit';
 }
 
 const props = withDefaults(defineProps<DButtonProps>(), {
-  variant: 'primary',
+  variant: 'default',
   danger: false,
   loading: false,
+  type: 'button',
 });
 
-// const isPrimary = computed(() => props.variant === 'primary');
-// const isSecondary = computed(() => props.variant === 'secondary');
-// const isTertiary = computed(() => props.variant === 'tertiary');
+const emit = defineEmits<{
+  click: [event: MouseEvent];
+}>();
+
+const isDefault = computed(() => props.variant === 'default');
+const isPrimary = computed(() => props.variant === 'primary');
+const isText = computed(() => props.variant === 'text');
+
 const isDanger = computed(() => props.danger === true);
+
+function onClick(e: MouseEvent) {
+  if (props.loading) {
+    e.preventDefault();
+    e.stopPropagation();
+    return;
+  }
+
+  emit('click', e);
+}
 </script>
 
 <template>
   <button
-    type="button"
-    class="rounded bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    v-if="isPrimary"
+    :type="props.type"
+    class="btn inline-flex items-center gap-x-2 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
     :class="[
       isDanger
-        ? 'bg-red-600 hover:bg-red-500 focus-visible:outline-red-600'
-        : 'bg-indigo-600 hover:bg-indigo-500 focus-visible:outline-indigo-600',
+        ? 'bg-danger-600 hover:bg-danger-500 focus-visible:outline-danger-600'
+        : 'bg-primary-600 hover:bg-primary-500 focus-visible:outline-primary-600',
+      { 'btn-disabled': props.loading },
     ]"
+    :aria-disabled="props.loading"
+    @click="onClick"
   >
-    <slot>Button text</slot>
+    <span v-if="$slots.icon || props.loading" class="-ml-0.5 h-5 w-5 text-white">
+      <svg
+        v-if="props.loading"
+        aria-label="Loading..."
+        role="status"
+        class="animate-spin"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      <slot v-else name="icon" />
+    </span>
+    <slot>Primary Button</slot>
+  </button>
+
+  <button
+    v-else-if="isText"
+    :type="props.type"
+    class="btn hover:underline focus:underline"
+    :class="[
+      isDanger
+        ? 'text-danger-600 hover:text-danger-500'
+        : 'text-primary-600 hover:text-primary-500',
+    ]"
+    @click="onClick"
+  >
+    <slot>Text Button</slot>
+  </button>
+
+  <button
+    v-else-if="isDefault"
+    :type="props.type"
+    class="btn bg-white shadow-sm ring-1 ring-inset"
+    :class="[
+      isDanger
+        ? 'text-danger-600 ring-danger-300 hover:bg-danger-50'
+        : 'ring-gray-300 hover:bg-gray-50',
+    ]"
+    @click="onClick"
+  >
+    <slot>Default Button</slot>
   </button>
 </template>
+
+<style>
+.btn {
+  @apply rounded px-3.5 py-2.5 text-sm font-semibold transition-colors duration-200 disabled:pointer-events-none disabled:opacity-60;
+}
+.btn-disabled {
+  @apply cursor-not-allowed opacity-60;
+}
+</style>
