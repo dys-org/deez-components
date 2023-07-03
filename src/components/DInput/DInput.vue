aff90f655108fba1195cf3a05fc7291ca7052ef4
David Soards <david@soards.me>
2023-07-03 02:41:47 -0400

    replace red w danger

--- src/components/DInput/DInput.vue
+++ src/components/DInput/DInput.vue
@@ -36,7 +36,7 @@ const isError = computed(() => props.status === 'error');
       :for="$attrs.id as string || props.name"
     >
       <span class="font-medium">{{ props.label }}</span>
-      <span v-if="isError" class="block text-red-600 dark:text-red-500">
+      <span v-if="isError" class="block text-danger-600 dark:text-danger-500">
         {{ props.errorMessage || 'This field is invalid.' }}
       </span>
       <span v-else-if="props.description" class="block text-black/60 dark:text-white/60">
@@ -52,7 +52,7 @@ const isError = computed(() => props.status === 'error');
         class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset placeholder:text-black/40 focus:ring-2 focus:ring-inset disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-black/50 disabled:ring-gray-200 dark:bg-white/5 dark:placeholder:text-white/30 dark:disabled:bg-black/10 dark:disabled:text-white/[.35] sm:text-sm sm:leading-6"
         :class="[
           isError
-            ? 'pr-10 text-red-600 ring-red-500 focus:ring-red-500 dark:text-red-500'
+            ? 'pr-10 text-danger-600 ring-danger-500 focus:ring-danger-500 dark:text-danger-500'
             : 'ring-gray-300 focus:ring-primary-500 dark:ring-white/10 dark:focus:ring-primary-500',
         ]"
         @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
@@ -64,7 +64,7 @@ const isError = computed(() => props.status === 'error');
         v-if="isError"
         class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
       >
-        <IconAlertCircle class="h-5 w-5 text-red-500" aria-hidden="true" />
+        <IconAlertCircle class="h-5 w-5 text-danger-500" aria-hidden="true" />
       </div>
     </div>
   </div>
