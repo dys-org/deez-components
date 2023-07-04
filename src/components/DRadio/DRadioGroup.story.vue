aef9e34daa52652aa7004576f78ad885b415f020
David Soards <david@soards.me>
2023-07-04 10:21:45 -0400

    improve radio group

--- src/components/DRadio/DRadioGroup.story.vue
+++ src/components/DRadio/DRadioGroup.story.vue
@@ -8,14 +8,41 @@ const notificationMethods = [
   { id: 'push', label: 'Push notification', value: 'push' },
 ];
 
-const selected = ref('email');
+const chosen = ref('email');
 </script>
 
 <template>
   <Story>
     <Variant title="Default">
-      <DRadioGroup :options="notificationMethods" name="notifMethods" v-model="selected" />
-      <p class="mt-8 font-mono dark:text-white/40">Chosen: {{ selected }}</p>
+      <DRadioGroup
+        :options="notificationMethods"
+        name="notifMethods"
+        v-model="chosen"
+        legend="Notifications"
+        description="How would you prefer to be notified?"
+      />
+      <p class="mt-8 font-mono dark:text-white/40">Chosen: {{ chosen }}</p>
+    </Variant>
+    <Variant title="Inline">
+      <DRadioGroup
+        :options="notificationMethods"
+        name="notifMethods"
+        v-model="chosen"
+        legend="Notifications"
+        description="How would you prefer to be notified?"
+        inline
+      />
+      <p class="mt-8 font-mono dark:text-white/40">Chosen: {{ chosen }}</p>
+    </Variant>
+    <Variant title="Hidden Legend no description">
+      <DRadioGroup
+        :options="notificationMethods"
+        name="notifMethods"
+        v-model="chosen"
+        legend="Notifications"
+        hide-legend
+      />
+      <p class="mt-8 font-mono dark:text-white/40">Chosen: {{ chosen }}</p>
     </Variant>
   </Story>
 </template>
--- src/components/DRadio/DRadioGroup.vue
+++ src/components/DRadio/DRadioGroup.vue
@@ -5,13 +5,19 @@ export interface DRadioGroupProps {
   modelValue: string;
   name: string;
   options: { id: string; label: string; value: string }[];
+  legend: string;
+  description?: string;
+  inline?: boolean;
+  hideLegend?: boolean;
 }
 
 const props = withDefaults(defineProps<DRadioGroupProps>(), {});
 
 const emit = defineEmits(['update:modelValue']);
 
-const picked = computed({
+const describedBy = props.description ? `${props.name}Description` : undefined;
+
+const checked = computed({
   get() {
     return props.modelValue;
   },
@@ -22,19 +28,21 @@ const picked = computed({
 </script>
 
 <template>
-  <fieldset>
-    <legend class="text-base font-semibold">Notifications</legend>
-    <p class="text-sm text-black/60 dark:text-white/60">
-      How do you prefer to receive notifications?
+  <fieldset :aria-describedby="describedBy">
+    <legend class="text-base font-semibold" :class="{ 'sr-only': hideLegend }">
+      {{ props.legend }}
+    </legend>
+    <p v-if="props.description" class="text-sm text-black/60 dark:text-white/60" :id="describedBy">
+      {{ props.description }}
     </p>
-    <div class="mt-4 space-y-4">
+    <div class="mt-4 flex flex-col gap-x-10 gap-y-4" :class="{ 'sm:flex-row': props.inline }">
       <div v-for="opt in props.options" :key="opt.id" class="flex items-center">
         <input
           :id="opt.id"
           :value="opt.value"
           :name="props.name"
           type="radio"
-          v-model="picked"
+          v-model="checked"
           class="h-4 w-4 border-gray-300 text-primary-600 focus:ring-primary-600 dark:border-white/30 dark:bg-white/5 dark:checked:bg-current"
         />
         <label :for="opt.id" class="ml-3 block text-sm font-medium leading-6">
