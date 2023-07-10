86f188b46141edb900f51c9ae505713ef0125e75
David Soards <david@soards.me>
2023-07-10 02:55:37 -0400

    add icons to autocomplete

--- src/components/DAutoComplete/DAutoComplete.story.vue
+++ src/components/DAutoComplete/DAutoComplete.story.vue
@@ -1,64 +1,108 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import DAutoComplete from './DAutoComplete.vue';
-import IconSearch from '~icons/feather/search';
+import DAutoComplete, { type Option } from './DAutoComplete.vue';
 import { logEvent } from 'histoire/client';
 
-const person = ref(null);
+import IconSearch from '~icons/feather/search';
+import IconPaperclip from '~icons/feather/paperclip';
+import IconZap from '~icons/feather/zap';
+import IconAperture from '~icons/feather/aperture';
 
+const selected = ref<Option | null>(null);
+
+const basic = [
+  { id: 1, label: 'David Youngquist Soards', description: 'Software Engineer' },
+  { id: 2, label: 'Gretel Durruty Soards', description: 'OR Nurse' },
+  { id: 3, label: 'Sebastian Alexander Soards', description: 'Toddler' },
+];
 const people = [
   {
     id: 1,
-    name: 'David Youngquist Soards',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    label: 'David Youngquist Soards',
+    description: 'Software Engineer',
+    left: {
+      imgURL:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
   },
   {
     id: 2,
-    name: 'Gretel Durruty Soards',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    label: 'Gretel Durruty Soards',
+    description: 'OR Nurse',
+    left: {
+      imgURL:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
   },
   {
     id: 3,
-    name: 'Sebastian Alexander Soards',
-    imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    label: 'Sebastian Alexander Soards',
+    description: 'Toddler',
+    left: {
+      imgURL:
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
   },
+];
+const things = [
+  { id: 'pa', label: 'Paperclip', left: { icon: IconPaperclip } },
+  { id: 'za', label: 'Zap', left: { icon: IconZap } },
+  { id: 'ap', label: 'Aperture', left: { icon: IconAperture } },
 ];
 </script>
 
 <template>
   <Story>
-    <Variant title="Default">
+    <Variant title="Basic">
+      <DAutoComplete
+        label="Assign to"
+        :options="basic"
+        v-model="selected"
+        @update:modelValue="logEvent('autocomplete update', $event)"
+      />
+      <p class="mt-8 font-mono">Label Value: {{ selected?.label }}</p>
+    </Variant>
+    <Variant title="Images">
       <DAutoComplete
         label="Assign to"
         :options="people"
-        v-model="person"
+        v-model="selected"
         @update:modelValue="logEvent('autocomplete update', $event)"
       />
+      <p class="mt-8 font-mono">ID Value: {{ selected?.id }}</p>
+    </Variant>
+    <Variant title="Icons">
+      <DAutoComplete
+        label="Check these out"
+        :options="things"
+        v-model="selected"
+        @update:modelValue="logEvent('autocomplete update', $event)"
+      />
+      <p class="mt-8 font-mono">Value: {{ selected }}</p>
     </Variant>
     <Variant title="Hidden Label">
       <DAutoComplete
         label="Assign to"
         :options="people"
-        v-model="person"
+        v-model="selected"
         @update:modelValue="logEvent('autocomplete update', $event)"
         hideLabel
         placeholder="Type the person's name"
       />
+      <p class="mt-8 font-mono">Label Value: {{ selected?.label }}</p>
     </Variant>
     <Variant title="With icon (Search)">
       <DAutoComplete
         label="Assign to"
         :options="people"
-        v-model="person"
+        v-model="selected"
         @update:modelValue="logEvent('autocomplete update', $event)"
         hideLabel
         placeholder="Search"
       >
         <template #icon> <IconSearch class="h-5 w-5" aria-hidden="true" /></template>
       </DAutoComplete>
+      <p class="mt-8 font-mono">Label Value: {{ selected?.label }}</p>
     </Variant>
   </Story>
 </template>
--- src/components/DAutoComplete/DAutoComplete.vue
+++ src/components/DAutoComplete/DAutoComplete.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, ref, type Component } from 'vue';
 import {
   Combobox,
   ComboboxButton,
@@ -14,9 +14,13 @@ import IconChevronDown from '~icons/feather/chevron-down';
 import IconX from '~icons/feather/x';
 
 export interface Option {
-  id: number;
-  name: string;
-  imageUrl: string;
+  id: number | string;
+  label: string;
+  description?: string;
+  left?: {
+    imgURL?: string;
+    icon?: string | Component;
+  };
 }
 
 export interface DAutoCompleteProps {
@@ -49,7 +53,10 @@ const filtered = computed(() =>
   query.value === ''
     ? props.options
     : props.options.filter((opt) => {
-        return opt.name.toLowerCase().includes(query.value.toLowerCase());
+        return (
+          opt.label.toLowerCase().includes(query.value.toLowerCase()) ||
+          opt.description?.toLowerCase().includes(query.value.toLowerCase())
+        );
       }),
 );
 
@@ -64,24 +71,24 @@ function onClear() {
       {{ props.label }}
     </ComboboxLabel>
     <div class="relative mt-2">
-      <div class="absolute inset-y-0 left-2 flex items-center text-black/40 dark:text-white/40">
+      <div class="absolute inset-y-0 left-2 flex items-center text-gray-400 dark:text-gray-500">
         <slot name="icon"></slot>
       </div>
       <ComboboxInput
         :class="[
-          'w-full rounded-md border-0 bg-white py-1.5 pl-3 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary-600 dark:bg-white/5 dark:ring-white/10 dark:placeholder:text-white/30 dark:focus:ring-primary-500 dark:disabled:bg-black/10 dark:disabled:text-white/[.35] sm:text-sm sm:leading-6',
+          'w-full rounded-md border-0 bg-white py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary-600 dark:bg-white/5 dark:ring-white/10 dark:placeholder:text-white/30 dark:focus:ring-primary-500 dark:disabled:bg-black/10 dark:disabled:text-white/[.35] sm:text-sm sm:leading-6',
           selected ? 'pr-14' : 'pr-10',
-          $slots.icon && 'pl-10',
+          $slots.icon ? 'pl-9' : 'pr-3',
         ]"
         @change="query = $event.target.value"
-        :displayValue="(opt) => (opt as Option)?.name"
+        :displayValue="(opt) => (opt as Option)?.label"
         :placeholder="placeholder"
         ref="inputRef"
       />
       <ComboboxButton
         class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
       >
-        <IconChevronDown class="h-5 w-5 text-black/40 dark:text-white/40" aria-hidden="true" />
+        <IconChevronDown class="h-5 w-5 text-gray-400 dark:text-gray-500" aria-hidden="true" />
       </ComboboxButton>
 
       <ComboboxOptions
@@ -102,9 +109,32 @@ function onClear() {
             ]"
           >
             <div class="flex items-center">
-              <img :src="opt.imageUrl" alt="" class="h-6 w-6 flex-shrink-0 rounded-full" />
-              <span :class="['ml-3 truncate', selected && 'font-semibold']">
-                {{ opt.name }}
+              <img
+                v-if="opt.left?.imgURL"
+                :src="opt.left.imgURL"
+                alt=""
+                class="mr-3 h-6 w-6 flex-shrink-0 rounded-full"
+              />
+              <component
+                v-else-if="opt.left?.icon"
+                :is="opt.left.icon"
+                :class="[
+                  'mr-3 h-3.5 w-3.5 ',
+                  active ? 'text-white' : 'text-gray-400 dark:text-gray-500',
+                ]"
+                aria-hidden="true"
+              />
+              <span :class="['truncate', selected && 'font-semibold']">
+                {{ opt.label }}
+              </span>
+              <span
+                v-if="opt.description"
+                :class="[
+                  'ml-2 truncate',
+                  active ? 'text-white/60' : 'text-black/40 dark:text-white/40',
+                ]"
+              >
+                {{ opt.description }}
               </span>
             </div>
 
@@ -127,7 +157,7 @@ function onClear() {
           type="button"
           @click="onClear()"
           aria-label="Clear"
-          class="p-0.5 text-lg text-black/60 hover:bg-gray-50 dark:text-white/60 dark:hover:bg-white/5"
+          class="rounded-full p-0.5 text-lg text-primary-600 transition-colors hover:bg-black/5 hover:text-primary-700 dark:text-primary-500 dark:hover:bg-white/5 dark:hover:text-primary-400"
         >
           <IconX class="h-4 w-4" aria-hidden="true" />
         </button>
