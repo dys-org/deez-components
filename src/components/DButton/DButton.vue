229d543c53b522bb59682255e8427437fb6c8497
David Soards <david@soards.me>
2023-10-01 15:08:12 -0400

    add tailwind-merge

--- src/components/DButton/DButton.vue
+++ src/components/DButton/DButton.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 import { computed, type ButtonHTMLAttributes } from 'vue';
+import { twMerge } from 'tailwind-merge';
 
 export interface DButtonProps {
   variant?: 'default' | 'primary' | 'link';
   danger?: boolean;
   loading?: boolean;
   type?: ButtonHTMLAttributes['type'];
-  size?: 'default' | 'small' | 'large';
+  size?: 'small' | 'large';
 }
+
+defineOptions({ inheritAttrs: false });
 
 const props = withDefaults(defineProps<DButtonProps>(), {
   variant: 'default',
   danger: false,
   loading: false,
   type: 'button',
-  size: 'default',
 });
 
 const emit = defineEmits<{
@@ -22,11 +24,10 @@ const emit = defineEmits<{
 }>();
 
 // static computed values
-const isDefaultVariant = props.variant === 'default';
+const isDefault = props.variant === 'default';
 const isPrimary = props.variant === 'primary';
 const isLink = props.variant === 'link';
 const isDanger = props.danger === true;
-const isDefaultSize = props.size === 'default';
 const isSmall = props.size === 'small';
 const isLarge = props.size === 'large';
 
@@ -45,34 +46,35 @@ function onClick(e: MouseEvent) {
 <template>
   <button
     :type="props.type"
-    :class="[
-      'inline-flex items-center gap-x-2 rounded font-semibold transition-colors duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500 disabled:pointer-events-none disabled:opacity-60',
-      {
-        'px-2 py-1 text-xs': isSmall,
-        'px-3 py-1.5 text-sm': isDefaultSize,
-        'px-4 py-2.5 text-base': isLarge,
+    :class="
+      twMerge(
+        'inline-flex items-center gap-x-2 rounded px-3 py-1.5 text-sm font-semibold transition-colors duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500 disabled:pointer-events-none disabled:opacity-60',
+        isLoading && 'cursor-not-allowed opacity-60',
 
-        'bg-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-white/10 dark:ring-transparent dark:hover:bg-white/[.15]':
-          isDefaultVariant,
-        'dark:text-white': isDefaultVariant && !isDanger,
-        'text-danger-600 dark:text-danger-500': isDefaultVariant && isDanger,
+        isSmall && 'px-2 py-1 text-xs',
+        isLarge && 'px-4 py-2.5 text-base',
 
-        'text-white shadow-sm': isPrimary,
-        'bg-primary-600 hover:bg-primary-700 dark:bg-primary-500 dark:hover:bg-primary-400':
-          isPrimary && !isDanger,
-        'bg-danger-600 hover:bg-danger-700 dark:bg-danger-500 dark:hover:bg-danger-400':
-          isPrimary && isDanger,
+        isDefault &&
+          'bg-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-white/10 dark:text-white dark:ring-transparent dark:hover:bg-white/[.15]',
+        isDefault && isDanger && 'text-danger-600 dark:text-danger-500',
 
-        'hover:underline focus:underline': isLink,
-        'text-primary-600 hover:text-primary-700 dark:text-primary-500 dark:hover:text-primary-400':
-          isLink && !isDanger,
-        'text-danger-600 hover:text-danger-700 dark:text-danger-500 dark:hover:text-danger-400':
-          isLink && isDanger,
+        isPrimary &&
+          'bg-primary-600 text-white shadow-sm hover:bg-primary-700 dark:bg-primary-500 dark:hover:bg-primary-400',
+        isPrimary &&
+          isDanger &&
+          'bg-danger-600 hover:bg-danger-700 dark:bg-danger-500 dark:hover:bg-danger-400',
 
-        'cursor-not-allowed opacity-60': isLoading,
-      },
-    ]"
+        isLink &&
+          'text-primary-600 hover:text-primary-700 hover:underline focus:underline dark:text-primary-500 dark:hover:text-primary-400',
+        isLink &&
+          isDanger &&
+          'text-danger-600 hover:text-danger-700 dark:text-danger-500 dark:hover:text-danger-400',
+
+        $attrs.class as string,
+      )
+    "
     :aria-disabled="isLoading || undefined"
+    v-bind="$attrs"
     @click="onClick"
   >
     <span v-if="$slots.iconStart || isLoading" class="-ml-0.5 h-5 w-5 text-current">
