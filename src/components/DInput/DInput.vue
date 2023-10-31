60e2a6754a92e5e0396a7003a3f61a6b707712ab
David Soards <david@soards.me>
2023-10-31 00:49:03 -0400

    add twMerge to input and slots

--- src/components/DInput/DInput.vue
+++ src/components/DInput/DInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
+import { twMerge } from 'tailwind-merge';
 
 import { DFormGroup } from '../DFormGroup';
 
@@ -52,23 +53,26 @@ const input = computed({
     :status="props.status"
     :error-message="props.errorMessage"
   >
-    <div :class="['relative', !props.hideLabel && 'mt-2']">
+    <div :class="['relative flex', !props.hideLabel && 'mt-2']">
+      <slot name="before" />
       <input
         v-bind="$attrs"
         :id="props.id"
         v-model.trim="input"
         :name="($attrs.name as string) || props.id"
         :type="isPassword ? (showPassword ? 'text' : props.type) : props.type"
-        class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset placeholder:text-black/40 focus:ring-2 focus:ring-inset disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-black/50 disabled:ring-gray-200 dark:bg-white/5 dark:placeholder:text-white/30 dark:disabled:bg-black/10 dark:disabled:text-white/[.35] sm:text-sm sm:leading-6"
-        :class="[
-          isError
-            ? 'text-danger-600 ring-danger-500 focus:ring-danger-500 dark:text-danger-500'
-            : 'ring-gray-300 focus:ring-primary-500 dark:ring-gray-600 dark:focus:ring-primary-500',
-          { 'pr-16': isPassword && isError },
-          { 'pr-10': isPassword || isError },
-        ]"
+        :class="
+          twMerge([
+            'block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset placeholder:text-black/40 focus:ring-2 focus:ring-inset disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-black/50 disabled:ring-gray-200 dark:bg-white/5 dark:placeholder:text-white/30 dark:disabled:bg-black/10 dark:disabled:text-white/[.35] sm:text-sm sm:leading-6',
+            isError
+              ? 'text-danger-600 ring-danger-500 focus:ring-danger-500 dark:text-danger-500'
+              : 'ring-gray-300 focus:ring-primary-500 dark:ring-gray-600 dark:focus:ring-primary-500',
+            isPassword && 'pr-10',
+            $attrs.class as string,
+          ])
+        "
         :aria-invalid="isError"
-        :aria-errormessage="isError && props.hideLabel ? `${id}ErrorMessage` : undefined"
+        :aria-describedby="isError && props.hideLabel ? `${id}ErrorMessage` : undefined"
       />
       <div class="absolute inset-y-0 right-0 mr-2 flex items-center">
         <!-- show/hide password -->
@@ -77,6 +81,7 @@ const input = computed({
           <IconEye v-else class="h-5 w-5" aria-hidden="true" />
         </button>
       </div>
+      <slot name="after" />
     </div>
   </DFormGroup>
 </template>
