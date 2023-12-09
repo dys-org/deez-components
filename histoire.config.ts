e056bccbedf78b8597f27f0f494a5ed1589b1190
David Soards <david@soards.me>
2023-12-09 14:30:28 -0500

    customize histoire 1

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,7 +1,22 @@
 import { HstVue } from '@histoire/plugin-vue';
-import { defineConfig } from 'histoire';
+import { defaultColors, defineConfig } from 'histoire';
 
 export default defineConfig({
   plugins: [HstVue()],
   setupFile: '/sandbox/histoire.setup.ts',
+  theme: {
+    title: 'deez-components',
+    logo: {
+      square: '/src/assets/img/DYS_white_on_red.svg',
+      light: '/src/assets/img/DYS_light.svg',
+      dark: '/src/assets/img/DYS_dark.svg',
+    },
+    dark: '/src/assets/img/DYS_dark.svg',
+    // logoHref: 'https://acme.com',
+    // favicon: './favicon.ico',
+    colors: {
+      gray: defaultColors.gray,
+      primary: defaultColors.teal,
+    },
+  },
 });
